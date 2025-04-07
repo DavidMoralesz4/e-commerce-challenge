af152003d3e5de2fa1be513352f5c366e2012ec4
DavidMoralesz4 <jd3285201@gmail.com>
2025-04-07 07:06:44 -0500

    getProductsService - consultar todos los productosde la base de datos

--- backend/src/services/productsService.ts
+++ backend/src/services/productsService.ts
@@ -1,2 +1,27 @@
+import { ProductModel } from "../config/data-source"
 
 /// Servicio para mostrar el producto
+export const getProductsService = async() => {
+    try {
+        const products = await ProductModel.find();
+        return products
+    } catch (error: unknown) {
+        if(error instanceof Error) {
+            throw new Error(error.message);
+        }
+    }
+}
+
+
+export const createProductService = async() => {
+    try {
+        const products = ProductModel.create({
+            
+        });
+        return products
+    } catch (error: unknown) {
+        if(error instanceof Error) {
+            throw new Error(error.message);
+        }
+    }
+}
