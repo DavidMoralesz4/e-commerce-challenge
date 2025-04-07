13062274a6a7a3ac9455d806a646221784c2058e
DavidMoralesz4 <jd3285201@gmail.com>
2025-04-07 07:42:04 -0500

    getProductsController - Obtener todos los productos

--- backend/src/controllers/productsController.ts
+++ backend/src/controllers/productsController.ts
@@ -1,11 +1,13 @@
 import { Request, Response } from "express";
+import { createProductService, getProductsService } from "../services/productsService";
 
 
 // Controllador para obtener todos los productos
-export const getProductController = (_: Request, res: Response) => {
+export const getProductController = async(_: Request, res: Response) => {
     try {
         // caso de exito, envio una respuesta con el producto
-        res.status(200).json('Todos los productos')
+        const products = await getProductsService()
+        res.status(200).json(products);
     } catch (error: unknown) {
         // Caso de error envio un mensaje con el error
         if(error instanceof Error) {
@@ -38,17 +40,35 @@ export const searchProductController = (_: Request, res: Response) => {
     }
 }
 
-export const createProductController = (_: Request, res: Response) => {
+export const createProductController = async (req: Request, res: Response) => {
+    const { name, price, stock, category, images, sizes, colors } = req.body;
+
     try {
-        // caso de exito, envio una respuesta con el producto
-        res.status(200).json('Crear un Producto')
+        // Validar los datos requeridos
+        if (!name || !price || stock === undefined || !category) {
+            return res.status(400).json({ message: "Faltan campos obligatorios" });
+        }
+
+        // Llamar al servicio para crear el producto
+        const newProduct = await createProductService({
+            name,
+            price,
+            stock,
+            category,
+            images,
+            sizes,
+            colors,
+        });
+
+        // Respuesta de éxito
+        res.status(201).json(newProduct);
     } catch (error: unknown) {
-        // Caso de error envio un mensaje con el error
-        if(error instanceof Error) {
-            res.status(500).json({message: error.message});
+        // Manejo de errores
+        if (error instanceof Error) {
+            res.status(500).json({ message: error.message });
         }
     }
-}
+};
 
 export const deleteProductsController = (_: Request, res: Response) => {
     try {
